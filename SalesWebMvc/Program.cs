2f9d435910c9025bad9c451bdb5a30bfb40e4cbe
Felipe Motta <velox4x@gmail.com>
2024-05-18 14:00:05 -0300

    Implementing simple search

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
+builder.Services.AddScoped<SalesRecordService>();
 
 // Adicionando serviço de autorização
 builder.Services.AddAuthorization();
